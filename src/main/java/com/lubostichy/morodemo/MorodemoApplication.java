6012f5a19fef5070d1b212760a139da1b86397c3
Ľuboš Tichý <lubostichy@bitbucket.org>
2019-08-24 09:41:36 +0200

    initial commit

---
+++ src/main/java/com/lubostichy/morodemo/MorodemoApplication.java
@@ -0,0 +1,13 @@
+package com.lubostichy.morodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorodemoApplication.class, args);
+	}
+
+}
